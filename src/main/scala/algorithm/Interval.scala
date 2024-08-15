b241f3532dc3473af729ca790504c3f54ffbe157
Amadeusz Kosik <calzone-coir-0m@icloud.com>
2024-08-15 12:04:13 +0200

    Rename packages to fix funny IDE behaviour.

--- src/main/scala/algorithm/Interval.scala
+++
@@ -1,9 +0,0 @@
-package com.eternalsh.interwalled
-package algorithm
-
-
-case class Interval[T](
-  start: Long,
-  end: Long,
-  value: T
-)
---
+++ src/main/scala/me/kosik/interwalled/Interwalled.scala
@@ -0,0 +1,21 @@
+
+package me.kosik.interwalled
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+
+object Interwalled {
+
+  private val logger: Logger =
+    Logger.getLogger(this.getClass.getCanonicalName)
+
+  def register(sparkSession: SparkSession): Unit = {
+    logger.info("Registering Interwalled extensions.")
+
+    sparkSession.experimental.extraStrategies = Seq(
+//      new IntervalTreeJoinStrategyOptim(spark),
+//      new GenomicIntervalStrategy(spark)
+    )
+  }
+
+}
