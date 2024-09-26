a95cbd4bf57475bd0ceab47d50a6263ec155f51a
Amadeusz Kosik <calzone-coir-0m@icloud.com>
2024-09-26 18:42:06 +0200

    Fix AIList with multiple components.

--- src/test/scala/me/kosik/interwalled/algorithm/ailist/BasicCorrectnessTest.scala
+++ src/test/scala/me/kosik/interwalled/algorithm/ailist/BasicCorrectnessTest.scala
@@ -2,6 +2,8 @@ package me.kosik.interwalled.algorithm.ailist
 
 import me.kosik.interwalled.algorithm.Interval
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.contain
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 import scala.jdk.CollectionConverters._
 
@@ -27,28 +29,6 @@ class BasicCorrectnessTest extends AnyFunSuite {
 
     val aiList = {
       val aiListBuilder = new AIListBuilder[String](10, 20, 10, 64)
-      lhs.foreach(aiListBuilder.put)
-      aiListBuilder.build()
-    }
-
-    val expected = (1 to 100) map { i => Interval(i, i, "L") -> Interval(i, i, "R")}
-    val actual = rhs.flatMap(interval => aiList.overlapping(interval).asScala.map(_ -> interval))
-
-    assert(actual.toArray sameElements expected, "All LHS should be present in the results set.")
-    assert(actual.length == lhs.length, "Each LHS should be exactly once in the results set.")
-  }
-
-  test("Correctness test - 1:1 matching, multiple list components.") {
-    val lhs = (1 to 10) flatMap ( i => {
-      val startOffsets = Array(1, 3, 5, 9, 11, 13, 15, 16, 17, 18, 19, 22, 24, 28, 32)
-      val endOffsets   = Array(2, 8, 7, 9, 60, 15, 19, 20, 19, 20, 21, 64, 63, 47, 58)
-
-      startOffsets.zip(endOffsets).map { case (start, end) => Interval(i * 100 + start, i * 100 + end, "L") }
-    })
-    val rhs = (1 to 10) flatMap { j => (1 to 10) map { i => Interval(i + 100 * (10 - j), i + 100 * (10 - j), "R")} }
-
-    val aiList = {
-      val aiListBuilder = new AIListBuilder[String](20, 5, 5, 5)
       lhs.foreach(aiListBuilder.put)
       aiListBuilder.build()
     }
@@ -92,5 +72,28 @@ class BasicCorrectnessTest extends AnyFunSuite {
     assert(result.map(_._1).toSet == Set(lhs), s"All LHS in the results set should be equal to the $lhs.")
     assert(result.map(_._2).toSet == rhs.toSet, "All RHS should be present in the results set.")
     assert(result.length == rhs.length, "Each RHS should be exactly once in the results set.")
+  }
+
+  test("Correctness test - 1 right interval matching all left intervals, multiple list components.") {
+
+    val lhs = ((1 to 100) map { i =>
+      Interval(i, i + 1, "L")
+    }) ++ ((1 to 100) map { i =>
+      Interval(i, i + 50, "L")
+    })
+
+    val rhs = List(Interval(0, 10000, "R"))
+
+    val aiList = {
+      val aiListBuilder = new AIListBuilder[String](20, 10, 4, 5)
+      lhs.foreach(aiListBuilder.put)
+      aiListBuilder.build()
+    }
+
+    val expected = lhs.map(i => i -> Interval(0, 10000, "R"))
+    val actual = rhs.flatMap(interval => aiList.overlapping(interval).asScala.map(_ -> interval))
+
+    assert(aiList.getComponentsCount == 2, "This test requires AIList with two components.")
+    actual.toSet should contain theSameElementsAs expected
   }
 }
