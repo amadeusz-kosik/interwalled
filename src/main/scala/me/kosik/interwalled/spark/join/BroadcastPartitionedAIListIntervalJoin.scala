e139602321f0f79da084e8554f8595035b934dfb
Amadeusz Kosik <git.stunning853@passmail.com>
2024-12-02 16:04:56 +0100

    Add BroadcastPartitionedAIListIntervalJoin implementation.

---
+++ src/main/scala/me/kosik/interwalled/spark/join/BroadcastPartitionedAIListIntervalJoin.scala
@@ -0,0 +1,54 @@
+package me.kosik.interwalled.spark.join
+
+import me.kosik.interwalled.ailist.{AIList, AIListBuilder}
+import me.kosik.interwalled.domain.{Interval, IntervalColumns, IntervalsPair}
+import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession}
+import org.apache.spark.sql.{functions => f}
+
+import scala.annotation.nowarn
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.reflect.runtime.universe._
+
+
+object BroadcastPartitionedAIListIntervalJoin extends IntervalJoin {
+
+  override def join[T : TypeTag](lhsInput: Dataset[Interval[T]], rhsInput: Dataset[Interval[T]]): Dataset[IntervalsPair[T]] = {
+    implicit val spark: SparkSession = lhsInput.sparkSession
+
+    @nowarn implicit val iTT = typeTag[Interval[T]]
+    implicit val iEncoder: Encoder[Interval[T]] = Encoders.product[Interval[T]]
+
+    @nowarn implicit val ipTT = typeTag[IntervalsPair[T]]
+    implicit val ipEncoder: Encoder[IntervalsPair[T]] = Encoders.product[IntervalsPair[T]]
+
+    val aiLists: Map[String, AIList[T]] = lhsInput
+      .repartition(f.col(IntervalColumns.KEY))
+      .rdd
+      .groupBy(_.key)
+      .map { case (key, intervals) =>
+        val aiListBuilder = new AIListBuilder[T]()
+        intervals.foreach(aiListBuilder.put)
+        key -> aiListBuilder.build()
+      }
+      .collect()
+      .toMap
+
+    val intervalListsBroadcast = spark.sparkContext
+      .broadcast(aiLists)
+
+    val joinedRDD = rhsInput.mapPartitions( _.flatMap { rhsInterval =>
+      intervalListsBroadcast.value.get(rhsInterval.key) match {
+        case Some(aiList) =>
+          aiList
+            .overlapping(rhsInterval)
+            .asScala
+            .map(lhsInterval => IntervalsPair(lhsInterval.key, lhsInterval, rhsInterval))
+
+        case None =>
+          Iterator.empty
+      }
+    })
+
+    joinedRDD.toDF().as[IntervalsPair[T]]
+  }
+}
