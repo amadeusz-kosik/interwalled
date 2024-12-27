a8761d4669d2109cf88e8b24af0f2a98dc7b1dd9
Amadeusz Kosik <git.stunning853@passmail.com>
2024-12-27 19:03:16 +0100

    Add MinMax option for pre-filtering intervals in broadcast implementation.

---
+++ src/main/scala/me/kosik/interwalled/ailist/MinMaxAIListBuilder.scala
@@ -0,0 +1,27 @@
+package me.kosik.interwalled.ailist
+
+import me.kosik.interwalled.domain.Interval
+
+class MinMaxAIListBuilder[T] {
+
+  private val builder = new AIListBuilder[T]()
+  private var minValue: Option[Long] = None
+  private var maxValue: Option[Long] = None
+
+  def build(): MinMaxAIList[T] = {
+    val aiList = builder.build()
+    MinMaxAIList(aiList, minValue, maxValue)
+  }
+
+  def put(interval: Interval[T]): Unit = {
+    builder.put(interval)
+
+    minValue = handleNone[Long](Math.min)(minValue, interval.from)
+    maxValue = handleNone[Long](Math.max)(maxValue, interval.to)
+  }
+
+
+
+  private def handleNone[T1](f: (T1, T1) => T1)(lhs: Option[T1], rhs: T1): Option[T1] =
+    Some(f(lhs.getOrElse(rhs), rhs))
+}
