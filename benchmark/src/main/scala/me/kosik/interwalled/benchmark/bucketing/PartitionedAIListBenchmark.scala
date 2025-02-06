a36f0308ab4ac341bc30e315c270f388ca3cae04
Amadeusz Kosik <amadeusz.kosik@icloud.com>
2025-02-06 15:40:51 +0100

    Rollup benchmark module into one entrypoint.

--- benchmark/src/main/scala/me/kosik/interwalled/benchmark/bucketing/PartitionedAIListBenchmark.scala
+++ benchmark/src/main/scala/me/kosik/interwalled/benchmark/bucketing/PartitionedAIListBenchmark.scala
@@ -1,39 +1,15 @@
 package me.kosik.interwalled.benchmark.bucketing
 
-import me.kosik.interwalled.benchmark.{Benchmark, Timer}
-import me.kosik.interwalled.domain.Interval
-import me.kosik.interwalled.spark.join.SparkNativeIntervalJoin
-import org.apache.spark.sql.{Dataset, SparkSession}
+import me.kosik.interwalled.benchmark.{Benchmark, BenchmarkArguments}
+import me.kosik.interwalled.spark.join.{IntervalJoin, PartitionedAIListIntervalJoin}
 
-import scala.util.Try
 
-object PartitionedAIListBenchmark extends Benchmark {
+object PartitionedAIListBenchmark extends Benchmark[PartitionedAIListBenchmarkArguments] {
 
-  override def run(
-    database: Dataset[Interval[String]],
-    query: Dataset[Interval[String]],
-    spark: SparkSession,
-    extraArguments: Array[String]
-  ): Unit = {
-    val buckets = extraArguments(0).split(",").map(_.toLong)
-
-    val results = buckets map { bucketsCount =>
-      val joinImplementation = new SparkNativeIntervalJoin(bucketsCount)
-      val timer = Timer.start()
-
-      Try {
-        val result = joinImplementation.join(database, query)
-        result.foreach(_ => ())
-
-        val msElapsed = timer.millisElapsed()
-        s"PartitionedAIList for $bucketsCount buckets took $msElapsed ms."
-      } getOrElse {
-        val msElapsed = timer.millisElapsed()
-        s"PartitionedAIList failed for $bucketsCount buckets in $msElapsed ms."
-      }
-    }
-
-    println("Results:")
-    results.foreach(r => println(s"\t${r}"))
-  }
+  override def prepareJoin(arguments: PartitionedAIListBenchmarkArguments): IntervalJoin =
+    new PartitionedAIListIntervalJoin(arguments.bucketSize)
 }
+
+case class PartitionedAIListBenchmarkArguments(
+  bucketSize: Long
+) extends BenchmarkArguments
--- benchmark/src/main/scala/me/kosik/interwalled/benchmark/bucketing/SparkNativeBucketingBenchmark.scala
+++ benchmark/src/main/scala/me/kosik/interwalled/benchmark/bucketing/SparkNativeBucketingBenchmark.scala
@@ -1,38 +1,14 @@
 package me.kosik.interwalled.benchmark.bucketing
 
-import me.kosik.interwalled.benchmark.{Benchmark, Timer}
-import me.kosik.interwalled.domain.Interval
-import me.kosik.interwalled.spark.join.SparkNativeIntervalJoin
-import org.apache.spark.sql.{Dataset, SparkSession}
+import me.kosik.interwalled.benchmark.{Benchmark, BenchmarkArguments}
+import me.kosik.interwalled.spark.join.{IntervalJoin, SparkNativeIntervalJoin}
 
-import scala.util.Try
+object SparkNativeBucketingBenchmark extends Benchmark[SparkNativeBucketingBenchmarkArguments] {
 
-object SparkNativeBucketingBenchmark extends Benchmark {
-
-  override def run(
-    database: Dataset[Interval[String]],
-    query: Dataset[Interval[String]],
-    spark: SparkSession,
-    extraArguments: Array[String]
-  ): Unit = {
-    val buckets = extraArguments(0).split(",").map(_.toLong)
-
-    buckets foreach { bucketsCount =>
-      val joinImplementation = new SparkNativeIntervalJoin(bucketsCount)
-      val timer = Timer.start()
-
-      Try {
-        val result = joinImplementation.join(database, query)
-        result.foreach(_ => ())
-
-        val msElapsed = timer.millisElapsed()
-        println(s"SparkNative for $bucketsCount buckets took $msElapsed ms.")
-      } getOrElse {
-        val msElapsed = timer.millisElapsed()
-        println(s"SparkNative failed for $bucketsCount buckets in $msElapsed ms.")
-      }
-
-    }
-
-  }
+  override def prepareJoin(arguments: SparkNativeBucketingBenchmarkArguments): IntervalJoin =
+    new SparkNativeIntervalJoin(arguments.bucketSize)
 }
+
+case class SparkNativeBucketingBenchmarkArguments(
+  bucketSize: Long
+) extends BenchmarkArguments
