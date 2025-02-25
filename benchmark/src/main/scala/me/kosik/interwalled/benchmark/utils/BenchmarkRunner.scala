d8d4c14e3358ede1ee4556a219d3532ad9dd2730
Amadeusz Kosik <amadeusz.kosik@icloud.com>
2025-02-25 22:38:30 +0100

    Turn off PartitionedAIListBenchmark due to GC errors.

--- benchmark/src/main/scala/me/kosik/interwalled/benchmark/utils/BenchmarkRunner.scala
+++ benchmark/src/main/scala/me/kosik/interwalled/benchmark/utils/BenchmarkRunner.scala
@@ -1,6 +1,6 @@
 package me.kosik.interwalled.benchmark.utils
 
-import me.kosik.interwalled.benchmark.TestData
+import me.kosik.interwalled.benchmark.{TestData, TestDataBuilder}
 import org.apache.spark.sql.SparkSession
 import org.slf4j.LoggerFactory
 
@@ -17,25 +17,25 @@ object BenchmarkRunner {
     testDataSizes: Seq[(Int, Long)],
     testDataSuites: Seq[String]
   ): Seq[BenchmarkResult] = {
-    val testDatas = for {
+    val testDataBuilders = for {
       (clustersCount, rowsCount)  <- testDataSizes
       testDataSuite               <- testDataSuites
-    } yield TestData.fromPath(testDataDirectory, testDataSuite, clustersCount, rowsCount) _
+    } yield TestDataBuilder(testDataDirectory, testDataSuite, clustersCount, rowsCount)
 
     for {
       benchmarkCallback <- benchmarks
-      testDataFactory   <- testDatas
-      result            <- run(sparkFactory, testDataFactory, benchmarkCallback)
+      testDataBuilder   <- testDataBuilders
+      result            <- run(sparkFactory, testDataBuilder, benchmarkCallback)
     } yield result
   }
 
   def run(
     sparkFactory: () => SparkSession,
-    testDataFactory: SparkSession => TestData,
+    testDatabuilder: TestDataBuilder,
     benchmarkCallback: BenchmarkCallback
   ): Option[BenchmarkResult] = Try {
     val spark = sparkFactory()
-    val testData = testDataFactory(spark)
+    val testData = testDatabuilder(spark)
 
     logger.info(s"Running benchmark - ${benchmarkCallback.description} on $testData")
     val result = benchmarkCallback.fn(testData)
@@ -45,7 +45,7 @@ object BenchmarkRunner {
     (spark, testData, result)
   } match {
     case Failure(exception) =>
-      logger.warn(s"Benchmark ${benchmarkCallback.description} failed flat: ${exception.getMessage.split('\n').head}")
+      logger.warn(s"Benchmark ${benchmarkCallback.description} on ${testDatabuilder} failed flat: ${exception.getMessage.split('\n').head}")
       Option.empty[BenchmarkResult]
 
     case Success((spark, testData, BenchmarkResult(_, _, _, _, _, Failure(_)))) =>
