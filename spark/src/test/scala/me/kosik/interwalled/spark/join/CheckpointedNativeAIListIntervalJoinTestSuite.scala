34477a9f2e40efcea95c3c606e9667caaca5aad9
Amadeusz Kosik <git.stunning853@passmail.com>
2025-11-26 09:11:15 +0100

    Enable all 'spark' module tests.

--- spark/src/test/scala/me/kosik/interwalled/spark/join/CheckpointedNativeAIListIntervalJoinTestSuite.scala
+++ spark/src/test/scala/me/kosik/interwalled/spark/join/CheckpointedNativeAIListIntervalJoinTestSuite.scala
@@ -2,13 +2,18 @@ package me.kosik.interwalled.spark.join
 
 import me.kosik.interwalled.spark.join.api.IntervalJoin
 import me.kosik.interwalled.spark.join.config.AIListConfig
+import me.kosik.interwalled.spark.join.implementation.ailist.NativeAIListConfig
+import me.kosik.interwalled.spark.join.implementation.ailist.implementation.CheckpointedNativeAIListIntervalJoin
+import me.kosik.interwalled.spark.join.preprocessor.PreprocessorConfig
+import me.kosik.interwalled.spark.join.preprocessor.bucketizer.BucketizerConfig
 
 
-//class CheckpointedNativeAIListIntervalJoinTestSuite extends AbstractIntervalJoinTestSuite {
-//
-//  override def intervalJoin: IntervalJoin = {
-//    val config = AIListConfig()
-//    new CheckpointedNativeAIListIntervalJoin("temporary/interwalled-chkpt", config, None)
-//  }
-//}
+class CheckpointedNativeAIListIntervalJoinTestSuite extends AbstractIntervalJoinTestSuite {
+
+  override def intervalJoin: IntervalJoin = {
+    val config = AIListConfig()
+    val preprocessorConfig = PreprocessorConfig.empty
+    new CheckpointedNativeAIListIntervalJoin(NativeAIListConfig(config, preprocessorConfig), "temporary/interwalled-chkpt")
+  }
+}
 
--- spark/src/test/scala/me/kosik/interwalled/spark/join/SparkNativeIntervalJoinTestSuite.scala
+++ spark/src/test/scala/me/kosik/interwalled/spark/join/SparkNativeIntervalJoinTestSuite.scala
@@ -1,10 +1,15 @@
 package me.kosik.interwalled.spark.join
 
 import me.kosik.interwalled.spark.join.api.IntervalJoin
-import me.kosik.interwalled.spark.join.implementation.spark.native.SparkNativeIntervalJoin
+import me.kosik.interwalled.spark.join.implementation.spark.native.{SparkNativeConfig, SparkNativeIntervalJoin}
+import me.kosik.interwalled.spark.join.preprocessor.PreprocessorConfig
+import me.kosik.interwalled.spark.join.preprocessor.bucketizer.BucketizerConfig
 
 
-//class SparkNativeIntervalJoinTestSuite extends AbstractIntervalJoinTestSuite {
-//
-//  override def intervalJoin: IntervalJoin = new SparkNativeIntervalJoin(None)
-//}
+class SparkNativeIntervalJoinTestSuite extends AbstractIntervalJoinTestSuite {
+
+  override def intervalJoin: IntervalJoin = {
+    val preprocessorConfig = PreprocessorConfig.empty
+    new SparkNativeIntervalJoin(SparkNativeConfig(preprocessorConfig))
+  }
+}
