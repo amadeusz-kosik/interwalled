b9e12980e74dd5301f5d35758fbb3bdb0f3d782d
Amadeusz Kosik <git.stunning853@passmail.com>
2025-12-09 18:01:13 +0100

    Remove unused ansible scripts.

--- ansible/roles/jdk/templates/java_home.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck shell=sh
-
-java_bin="$(update-alternatives --list java | head -n 1)"
-java_bin_directory="$(dirname "$java_bin")"
-java_home_directory="$(dirname "$java_bin_directory")"
-
-export JAVA_HOME="$java_home_directory"
