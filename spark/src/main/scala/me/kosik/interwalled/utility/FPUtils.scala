2f2ca7be23b5d5b84f23922eb7473290a04c4c7c
Amadeusz Kosik <git.stunning853@passmail.com>
2025-12-10 12:44:10 +0100

    Remove dead code.

--- spark/src/main/scala/me/kosik/interwalled/utility/FPUtils.scala
+++
@@ -1,9 +0,0 @@
-package me.kosik.interwalled.utility
-
-object FPUtils {
-
-  def flipOption[T1, T2](lhs: Option[T1], rhs: Option[T2]): Option[(T1, T2)] = (lhs, rhs) match {
-    case (Some(l), Some(r)) => Some(l -> r)
-    case _ => None
-  }
-}
