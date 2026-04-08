aa66f8598c3600fc60819e3e8d6e9e9569d44aa9
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-04-08 10:38:16 +0200

    Add missing doc comment for BenchmarkResult classess.

--- benchmarks/common/src/main/scala-2.12/me/kosik/interwalled/benchmark/common/results/model/BenchmarkResult.scala
+++ benchmarks/common/src/main/scala-2.12/me/kosik/interwalled/benchmark/common/results/model/BenchmarkResult.scala
@@ -2,6 +2,10 @@ package me.kosik.interwalled.benchmark.common.results.model
 
 import me.kosik.interwalled.benchmark.common.timer.TimerResult
 
+/* The [T] generic parameter is in fact Dataset / DataFrame from Apache Spark. It is substituted
+ *  with generic to evade adding Spark itself as the dependency of this module. This way
+ *  Sequila can be loaded with a different version of Spark than the whole project.
+ */
 
 sealed trait BenchmarkResult[T]
 
